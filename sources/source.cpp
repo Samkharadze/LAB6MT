f72feb30bc6dc12ae986872627da4b850c89a67a
Gosha <giorgisamh1771@gmail.com>
2019-12-07 10:36:01 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -6,16 +6,17 @@
 #include <ctime>
 #include <boost/log/utility/setup.hpp>
 #include <vector>
-int main(int argc, char*argv[]) {
+int main(int argc, char*argv[])
+{
     srand(time(NULL));
     //size_t count_threads = thread::hardware_concurrency();
     size_t count_threads = 2;
     if (argc > 1)
-        count_threads = 
+        count_threads=
 boost::lexical_cast<size_t>(argv[1]); 
 //преобразование в size_t
-    boost::log::add_file_log("Log.log"); //вывод в файл
-    boost::log::add_console_log(std::cout); //вывод в консоль
+    boost::log::add_file_log("Log.log");//вывод в файл
+    boost::log::add_console_log(std::cout);//вывод в консоль
     std::vector<std::thread> threads;
     for (size_t i = 0; i < count_threads; ++i)
     threads.emplace_back(SHA256::getting_a_hash); 
