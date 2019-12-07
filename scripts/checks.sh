d0c11dd0d9da8dfd21b84d89a255f42f7b59f013
Gosha <giorgisamh1771@gmail.com>
2019-12-07 10:02:49 +0300

    Upd111

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-files=`find . -name "sourse.cpp" -or -name "header.hpp" -or -name ".h" | grep -v "./tools/*"`
+files=`find . -name "source.cpp" -or -name "header.hpp" -or -name ".h" | grep -v "./tools/*"`
 filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo $files | xargs cpplint --filter=$filter
 
